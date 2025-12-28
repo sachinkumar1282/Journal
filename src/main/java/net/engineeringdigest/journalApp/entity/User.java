0bdedba1503b7201fc67837406de7d845ec2af7f
Sachin Kumar <sachisingh114@gmail.com>
2025-12-28 15:28:45 +0530

    intial commit

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -0,0 +1,34 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collation = "journal_user")
+@Data
+public class User {
+
+  @Id
+  private ObjectId id;
+
+  @Indexed(unique = true)
+  @NonNull
+  private String userName;
+  @NonNull
+  private String password;
+
+  @DBRef
+  private List<JournalEntry> journalEntries =new ArrayList<>();
+
+
+}
+
+
